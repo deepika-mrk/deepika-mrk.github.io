cab38659f07b416d692614a0a752fca440213fbb
deepika-mrk <108858409+deepika-mrk@users.noreply.github.com>
2022-07-07 13:33:38 +0530

    files added

---
+++ rectangle.js
@@ -0,0 +1,7 @@
+function area()
+{
+	var s1 = document.getElementById('val1').value;
+	var s2 = document.getElementById('val2').value;
+	var ar = s1 * s2 ;
+	document.getElementById('val3').value= ar;
+}
