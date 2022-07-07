22bfb935eaa24f9a24fb1ef9ea6b139151574568
deepika-mrk <108858409+deepika-mrk@users.noreply.github.com>
2022-07-07 12:35:58 +0530

    added 7th july files
    added 7th july files

---
+++ 7thjuly.js
@@ -0,0 +1,18 @@
+function submit()
+{
+	console.log('hello mirketa');
+	var num = document.getElementById('b1').value;
+	var mul =2 * num ;
+	console.log(mul);
+}
+
+
+
+function b2()
+{
+	var b1 = document.getElementById('b1').value;
+	var b2=2 * b1;
+	document.getElementById('result').textContent = 'Result is  ' + b2;
+}
+
+deepika-mrk.github.io
