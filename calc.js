a87cce2d3952a83b1a9ee182f8427532ecbca2de
deepika-mrk <108858409+deepika-mrk@users.noreply.github.com>
2022-07-09 19:33:01 +0530

    Added files

---
+++ calc.js
@@ -0,0 +1,27 @@
+function addition(){
+var num1= document.getElementById('value1').value;
+var num2 = document.getElementById('value2').value;
+var sum = parseInt(num1) + parseInt(num2) ;
+console.log(sum);
+}
+
+function subtraction(){
+var num1= document.getElementById('value1').value;
+var num2 = document.getElementById('value2').value;
+const sub = parseInt(num1) - parseInt(num2) ;
+console.log(sub);
+}
+
+function multiplication(){
+var num1= document.getElementById('value1').value;
+var num2 = document.getElementById('value2').value;
+const mul = parseInt(num1) * parseInt(num2) ;
+console.log(mul);
+}
+
+function division(){
+var num1= document.getElementById('value1').value;
+var num2 = document.getElementById('value2').value;
+const div = parseInt(num1) / parseInt( num2) ;
+console.log(div);
+}
